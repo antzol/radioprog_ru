8fd75b2b0923cc507b514df696387d8bd966d329
antzol <antzol@yandex.ru>
2021-01-03 18:39:40 +0300

    Add files via upload

---
+++ 988-cpp-unit-testing-with-qt-test-advanced/UnitTests/TestIntBitset/TestIntBitset.h
@@ -0,0 +1,24 @@
+#include "IntBitset.h"
+
+#include <QtTest>
+
+class TestIntBitset: public QObject
+{
+    Q_OBJECT
+
+private slots:
+    // -- setup/cleanup --
+    void cleanupTestCase();
+    void initTestCase();
+    void init();
+
+    // -- tests --
+    void testOperators();
+    void testSetAllOff();
+    void testSetAllOn();
+    void testSetOff();
+    void testSetOn();
+
+private:
+    IntBitset mBS;
+};
